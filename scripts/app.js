74bc293665ef45a491865e70d53719b7096666ee
Sujan Basnet <47745390+SBasnet24@users.noreply.github.com>
2020-04-04 22:31:40 +0545

    firstcommit

---
+++ scripts/app.js
@@ -0,0 +1,69 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon= document.querySelector('.icon img');
+
+
+const updateUI = (data)=>{
+    //destructuring the object since it has same name
+    const {cityDets, weather }= data;
+    // update Deatils Template
+     details.innerHTML =
+     `
+        <h5 id="cname" class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+         </div>
+     `;
+
+    // update the dayand night icon images
+    let timeSrc = weather.IsdayTime ? 'img/day.svg' :'img/night.svg' ;
+    time.setAttribute('src',timeSrc); 
+
+    // updating the icon packages
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`  ;
+    icon.setAttribute('src',iconSrc);
+
+
+     // removing the class d-none
+     if (card.classList.contains("d-none")) {
+        card.classList.remove("d-none");
+    }
+}
+
+
+
+
+const updateCity = async (city) => {
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    // returns the promise
+    return {cityDets,  weather };
+};
+
+
+cityForm.addEventListener('submit',e =>{
+    //preventDefaultAction
+    e.preventDefault();
+    // get city Value
+    const city = cityForm.city.value.trim();  
+    cityForm.reset();
+    updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err=>console.log(err));
+    // cityName.innerHTML = city;
+    // update the ui with the new city
+
+    //setLocalStorage
+    localStorage.setItem('city',city);
+     
+});
+
+if(localStorage.getItem('city')){
+    updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,18 @@
+const key = 'bPPATaZ7VMSb9hXr51AcdNUiJ9H24rYJ';
+//getWeatherInformation
+const getWeather = async(id) =>{
+    const base='http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base+query);
+    const data = await response.json();
+    return data[0];
+};
+// getCity Information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+};
+
